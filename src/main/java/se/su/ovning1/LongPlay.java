1d8b241d7e6643e514afca375c573313b449ab97
jononor <john.nordstrom500@outlook.com>
2026-04-03 04:37:10 +0900

    finished class

--- src/main/java/se/su/ovning1/LongPlay.java
+++ src/main/java/se/su/ovning1/LongPlay.java
@@ -1,4 +1,18 @@
 package se.su.ovning1;
 
-public class LongPlay {
+public class LongPlay extends Recording {
+    public LongPlay ( String name, String artist, int year, int condition, double price ) {
+        super ( name, artist, year, condition, price );
+    }
+
+    @Override
+    public String getType() {
+        return "LP";
+    }
+
+    @Override
+    public double getPrice() {
+        int presentYear = 2025;
+        return super.getPrice() + ((presentYear - getYear()) * 5.0);
+    }
 }
